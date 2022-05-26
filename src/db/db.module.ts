7255bd65a03087b631a633f3fae6a378bc54cbff
Prateek Banga <prateek.banga@techmojo.in>
2022-05-26 12:29:14 +0530

    removed log

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -9,7 +9,6 @@ import { MongoClient, Db } from 'mongodb';
       provide: 'MONGO_PROVIDER',
       useFactory: async (): Promise<Db> => {
         try {
-          console.log(process.env.MONGO_DB_URI)
           const client = await MongoClient.connect(process.env.MONGO_DB_URI);
           return client.db(process.env.MONGO_DB_NAME);
         } catch (e) {
